9aaaaa9e95c67a57ecb326e4992df3ab2d9b59af
Ieffe <novditha@gmail.com>
2020-11-27 17:14:07 +0800

    adding stylesheet

--- App.js
+++ App.js
@@ -8,93 +8,176 @@
 
 import * as React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   View,
   Text,
-  StatusBar,
   StyleSheet,
   TouchableOpacity,
   Button
 } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import {TextInput} from 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-// function loginScreen({navigation}){
-//   return(
-//     <View>
-//       <TextInput 
-//       />
+function loginScreen({navigation}){
+  return(
+    <View
+    style={styles.containerForms}
+    >
+      <Text
+        style={styles.loginTextTitle}
+      >
+        RPN
+      </Text>
+      <Text
+        style={styles.loginText}
+      >
+        The Roleplay Network
+      </Text>
+      <TextInput
+        placeholder='E-mail'
+        keyboardType='email-address'
+        placeholderTextColor='white'
+        style={styles.inputs}
+      />
 
-//       <TextInput 
-//       />
+      <TextInput 
+        placeholder='Password'
+        placeholderTextColor='white'
+        secureTextEntry={true}
+        style={styles.inputs}
+      />
 
-//       <TouchableOpacity>
-//         <Text>
+      <TouchableOpacity
+        onPress={()  => navigation.push('Home')}
+        style={styles.buttonsConfirm}
+      >
+        <Text 
+          style={styles.buttonTextConfirm}
+        >
+          Login
+        </Text>
+      </TouchableOpacity>
 
-//         </Text>
-//       </TouchableOpacity>
-//     </View>
-//   )
-// }
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Signup')}
+        style ={styles.loginNewAccountContainer}
+      >
+        <Text
+          style = {styles.loginNewAccountText}
+        >
+          Make a new account here!
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
 
-// function newAccountScreen({navigation}){
-//   return(
-//     <View>
-//       <TextInput
-//       placeholder='Insert new E-mail'
+function signUpScreen({navigation}){
+  return(
+    <View
+      style={styles.containerForms}
+    >
+      <TextInput
+      placeholder='Insert new E-mail'
+      placeholderTextColor='white'
+      style={styles.inputs}
       
-//       />
-//       <TextInput
-//       placeholder='Insert new password'
-      
-//       />
+      />
+      <TextInput
+      placeholder='Insert new password'
+      placeholderTextColor='white'
+      secureTextEntry={true}
+      style={styles.inputs}
+      />
 
-//       <TouchableOpacity 
-//         onPress={ }
-//       >
-//         <Text>
+      <TextInput
+      placeholder='Confirm password'
+      placeholderTextColor='white'
+      secureTextEntry={true}
+      style={styles.inputs}
+      />
 
-//         </Text>
-//       </TouchableOpacity>
-//     </View>
+      <TouchableOpacity 
+        onPress={() => navigation.goBack()}
+        style={styles.buttonsConfirm}
+      >
+        <Text 
+          style={styles.buttonTextConfirm}
+        >
+          Sign-up
+        </Text>
+      </TouchableOpacity>
+    </View>
     
-//   )
-// }
+  )
+}
 
 function homeScreen({navigation}){
   return(
-    <View>
-      <Text>
-        UTS Apps
+    <View
+      style={styles.container}
+    >
+      <Text 
+        style={styles.contentHeadTitle}
+      >
+        RPN Home
       </Text>
-      <Text>
+      <Text
+        style={styles.contentHeadSubtitle}
+      >
         This is the Home Screen 
       </Text>
-      <Button
-        title="About this app"
-        onPress={() => navigation.push('About')}
-      />
+      <TouchableOpacity
+        style = {styles.buttonMenu}
+        onPress={() => navigation.navigate('About')}
+      >
+        <Text
+          style = {styles.buttonText}
+        > 
+          About this Apps 
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style = {styles.buttonBack}
+        onPress={() => navigation.goBack()}
+      >
+        <Text
+          style = {styles.buttonText}
+        > 
+          Log out 
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 function aboutScreen({navigation}){
   return(
-    <View>
-      <Text>
+    <View
+      style={styles.container}
+    >
+      <Text
+        style = {styles.contentHeadTitle}
+      >
         About
       </Text>
-      <Text>
+      <Text
+        style = {styles.contentHeadSubtitle}
+      >
         This Apps Made by Novendra Krisna W. 
       </Text>
-      <Button
-        title="Return"
+      <TouchableOpacity
+        style = {styles.buttonBack}
         onPress={() => navigation.goBack()}
-      />
+      >
+        <Text
+          style = {styles.buttonText}
+        > 
+          Return 
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -103,6 +186,14 @@ function App() {
   return(
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={loginScreen}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={signUpScreen}
+        />
         <Stack.Screen
           name="Home"
           component={homeScreen}
@@ -118,102 +209,89 @@ function App() {
 
 const styles = StyleSheet.create({
   inputs: {
-
+    borderRadius: 30,
+    color: 'white',
+    paddingHorizontal: 16,
+    backgroundColor: '#ff5c4a',
+    marginVertical: 15,
+    fontSize: 16,
+    width: 250,
+    fontFamily: 'Arial'
   },
-  buttons: {
-
+  buttonsConfirm: {
+    width: 250,
+    borderRadius: 25,
+    backgroundColor: '#4aff92',
+    marginVertical: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    fontSize: 16
+  },
+  buttonMenu: {
+    width: 150,
+    borderRadius: 25,
+    backgroundColor: '#0f83ff',
+    marginVertical: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    fontSize: 16
+  },
+  buttonBack: {
+    width: 100,
+    borderRadius: 25,
+    backgroundColor: '#ff5c4a',
+    marginVertical: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    fontSize: 16
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white"
+  },
+  buttonTextConfirm: {
+    textAlign: "center",
+    color: "white"
   },
   container: {
-    
+    flex: 1,
+    backgroundColor: '#36454f',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  containerForms: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#36454f'
+  },
+  loginText:{
+    color: 'white',
+    fontSize: 12,
+    marginBottom: 30
+  },
+  loginTextTitle:{
+    color: 'white',
+    fontSize: 60
+  },
+  loginNewAccountContainer:{
+    marginVertical: 50
+  },
+  loginNewAccountText:{
+    color: 'white',
+    fontSize: 14,
+    textDecorationLine: 'underline'
+  },
+  contentHeadTitle:{
+    color: 'white',
+    fontSize: 32
+  },
+  contentHeadSubtitle:{
+    color: 'white',
+    fontSize: 16
   }
 })
 
 export default App;
-// const App: () => React$Node = () => {
-//   return (
-//     <>
-//       <StatusBar barStyle="dark-content" />
-//       <SafeAreaView>
-//         <ScrollView
-//           contentInsetAdjustmentBehavior="automatic"
-//           style={styles.scrollView}>
-//           <Header />
-//           {global.HermesInternal == null ? null : (
-//             <View style={styles.engine}>
-//               <Text style={styles.footer}>Engine: Hermes</Text>
-//             </View>
-//           )}
-//           <View style={styles.body}>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Step One</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
-//                 screen and then come back to see your edits.
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>See Your Changes</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <ReloadInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Debug</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <DebugInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Learn More</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Read the docs to discover what to do next:
-//               </Text>
-//             </View>
-//             <LearnMoreLinks />
-//           </View>
-//         </ScrollView>
-//       </SafeAreaView>
-//     </>
-//   );
-// };
 
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
-//   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
-//   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
-// });
-
-// export default App;
